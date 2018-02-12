f178ca3844b62e06b6cfe623c9125bb5a2b04b04
Alejandro Acevedo <07117043@galileo.edu>
2018-02-11 19:10:42 -0600

    Programa usando Switch Case

---
+++ Introduccion/VariablesEstructurasControl.js
@@ -0,0 +1,31 @@
+<script>
+var mes = 10; //variable establecida con el número 10 para el mes
+switch(mes){
+	case 12:
+	case 1:
+	case 2:
+		alert("Es invierno")
+		alert("Carné 07117043 || Alejandro Ernesto Acevedo Ovando")
+	break;
+	case 3:
+	case 4:
+	case 5:
+		alert("Es primavera")
+		alert("Carné 07117043 || Alejandro Ernesto Acevedo Ovando")
+	break;
+	case 6:
+	case 7:
+	case 8:
+		alert("Es verano")
+		alert("Carné 07117043 || Alejandro Ernesto Acevedo Ovando")
+	break;
+	case 9:
+	case 10:
+	case 11:
+		alert("Es otoño"
+		alert("Carné 07117043 || Alejandro Ernesto Acevedo Ovando")
+	break;
+	default:
+		alert("La variable aún no tiene un valor entre 1 y 12")
+}	
+<script>
